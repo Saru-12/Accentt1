5e80481bb1a7547d1c977fa0e31d6b9876cf99c5
Saru-12 <Sarutiwaskar@gmail.com>
2024-12-25 19:03:41 +0530

    new commit

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,10 +2,11 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "ecr_repository_url" {
-  description = "URL of the ECR repository"
-  default     = "<ECR_URL>"
+variable "image_url" {
+  description = "The URL of the ECR repository"
+  type        = string
 }
+
 
 variable "container_port" {
   description = "The container port to expose"
